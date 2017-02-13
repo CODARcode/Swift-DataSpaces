2f367aadc37508e495c09e8eb32ef7998d06e7c2
Philip Davis <philipdavis01@gmail.com>
2017-02-13 13:44:14 -0500

    added trivial client runs

--- dsTest.swift
+++ dsTest.swift
@@ -15,11 +15,22 @@ app(void signal) dataspaces_server(int nserver, int nclient) {
     "./dataspaces_server -s <<nserver>> -c <<nclient>>" 
 }
 
+@dispatch=WORKER
+app(void signal) test_writer() {
+    "./test_writer DATASPACES <<NX>> 3 1 1 1 128 128 128 50 1"
+}
+
+@dispatch=WORKER
+app(void signal) test_reader() {
+    "./test_reader DATASPACES <<NY>> 3 1 1 1 128 128 128 50 1"
+}
+
 foreach i in [0:n-1] {
     location L = location(i, HARD, RANK);
     @location=L dataspaces_server(NS, NC);
 }
 check_conf_exists() => {
-    //launch clients
+    @par=NX test_writer();
+    @par=NY test_reader();
 }
 
