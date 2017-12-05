50d6eb15d753d0745f1cb6b01fb8f3d3e4147c64
Justin Wozniak <wozniak@mcs.anl.gov>
2017-12-05 09:58:39 -0600

    Make test runner generic

--- swift/run-test-sds.sh
+++ swift/run-test-sds.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 set -eu
 
+# RUN TEST SDS
+# Runs Swift/T tests
+# The user provides the test number
+
+if [ ${#*} != 1 ]
+then
+  echo "Provide a test number!"
+  exit 1
+fi
+
+# Test number
+T=$1
+
+echo "Running Swift/T test $T ..."
+
 export SWIFT_PATH=$PWD
 export SDS_DEBUG=1
 
@@ -11,7 +26,7 @@ DS_SERVER_PID=${!}
 echo "dataspaces_server running: DS_SERVER_PID=$DS_SERVER_PID"
 
 # N ranks -> N-1 workers
-swift-t -l -n $CLIENTS test-sds-1.swift
+swift-t -l -n $CLIENTS test-sds-$T.swift
 
 echo "killing dataspaces_server"
 kill -1 $DS_SERVER_PID
