4e47565fb4547ed0a523c5fc8b5cbcbf35a079ef
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-12-08 10:39:09 -0600

    Insert DS server mgmt commands

--- swift/run-test-sds.sh
+++ swift/run-test-sds.sh
@@ -21,9 +21,18 @@ export SDS_DEBUG=1
 
 CLIENTS=3
 
+# Clean up from previous runs
+rm -f srv.lck conf *.log
+
 dataspaces_server -s 1 -c $CLIENTS &
 DS_SERVER_PID=${!}
 echo "dataspaces_server running: DS_SERVER_PID=$DS_SERVER_PID"
+
+## Give some time for the servers to load and startup
+while [ ! -f conf ]; do
+    sleep 1s
+done
+sleep 5s  # wait for server to fill in the conf file
 
 # N ranks -> N-1 workers
 swift-t -l -n $CLIENTS test-sds-$T.swift
