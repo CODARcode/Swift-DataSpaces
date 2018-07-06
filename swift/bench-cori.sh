cd70e07c851f9a74b1284d23426c0f2289ab731e
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-07-06 15:26:58 -0500

    Wozniak's bench-cori.sh

--- swift/bench-cori.sh
+++ swift/bench-cori.sh
@@ -1,14 +1,13 @@
-#!/bin/zsh -f
+#!/bin/bash -l
 set -eu
 
-# BENCH BLUES
-# Runs Swift/T bench-*.swift on Blues
+# BENCH CORI
+# Runs Swift/T T.swift on Cori
 # The user provides the test name and
-# sets env vars PROCS, DS_CLIENTS, and DS_SERVERS
-# such that PROCS = DS_CLIENTS + DS_SERVERS
-# We use ZSH for an array convenience at the end
+# sets env vars DS_CLIENTS and DS_SERVERS.
+# This script will set PROCS = DS_CLIENTS + DS_SERVERS
 
-if [ ${#*} != 3 ]
+if [[ ${#*} != 1 ]]
 then
   echo "Provide a test name!"
   exit 1
@@ -16,58 +15,71 @@ fi
 
 # Test name
 T=$1
-export DS_CLIENTS=$2
-export DS_SERVERS=$3
-((PROCS = $DS_CLIENTS + $DS_SERVERS))
-export PROCS
 
-#export TURBINE_LAUNCH_OPTIONS="-n $DS_CLIENTS -N $((DS_CLIENTS / PPN)) -c $((32 / PPN))"
-export TURBINE_LAUNCH_OPTIONS="-n $DS_CLIENTS -N $(((DS_CLIENTS+(PPN-1)) / PPN)) -c $((32 / PPN))"
-
-echo "Running Swift/T test $T..."
+echo "Running Swift/T test $T ..."
 
 export THIS=$( cd $( dirname $0 ) ; /bin/pwd )
 cd $THIS
+pwd
 
 # Find the SDS package
 export SWIFT_PATH=$PWD
 # Set SDS debug logging
 export SDS_DEBUG=0
 
-# Set up PATH
-PATH=/soft/jdk/1.8.0_51/bin:$PATH
-PATH=/home/wozniak/sfw/blues/login/swift-t-ds/stc/bin:$PATH
-PATH=/home/wozniak/sfw/blues/login/swift-t-ds/turbine/bin:$PATH
-which swift-t
+# Set up modules
+module load java
 
-export WALLTIME=00:10:00
+# Set up PATH
+SWIFT=~wozniak/Public/sfw/compute/swift-t-ds
+PATH=$SWIFT/stc/bin:$PATH
+PATH=$SWIFT/turbine/bin:$PATH
+
+if [[ ${DS_CLIENTS:-} == "" ]] || \
+   [[ ${DS_SERVERS:-} == "" ]]
+then
+  echo "Set DS_CLIENTS and DS_SERVERS!"
+  exit 1
+fi
 
 # Sanity check user settings
-if (( $DS_CLIENTS < 2 ))
+if ! (( $DS_CLIENTS > 1 ))
 then
   echo "User error!"
   echo "DS_CLIENTS=$DS_CLIENTS must be greater than 1."
   exit 1
 fi
-if (( $PROCS != $DS_CLIENTS + $DS_SERVERS ))
-then
-  echo "User error!"
-  echo PROCS=$PROCS "!=" DS_CLIENTS=$DS_CLIENTS + DS_SERVERS=$DS_SERVERS
-  exit 1
-fi
 
+# For bench-2, we want to set -Fwait-coalesce to disable coalescing
 OPTZ=${OPTZ:-}
-
-# export TURBINE_LOG=1
 
 # Swift/T headers to check for updates
 UPTODATE=( -U sds.swift -U make_data.swift -U sink.swift )
 
-stc $OPTZ ${UPTODATE} $T.swift
+which swift-t stc turbine
+
+# Run STC
+stc $OPTZ ${UPTODATE[@]} $T.swift
+
+# Turbine settings
 # Turbine will actually run on DS_CLIENTS processes!
+PROCS=$(( $DS_CLIENTS + $DS_SERVERS ))
+export WALLTIME=00:05:00
+
+export TURBINE_DIRECTIVE="#SBATCH -C knl,quad,cache\n#SBATCH --license=SCRATCH"
+export QUEUE=debug
+export PPN=1
+export TURBINE_LOG=0
+LD_LIBRARY_PATH=$HOME/Public/sfw/R-3.4.0/lib64/R/lib
+
+# Run Turbine
 set -x
-turbine -V -m slurm -l \
+turbine -m slurm -l \
         -n $PROCS \
         -i $THIS/init-ds.sh \
         -e SDS_DEBUG \
+        -e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
         $T.tic
+#         hi.tcl
+
+#        $T.tic
